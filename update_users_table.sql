66c2bfd85970152a4b0ea4f5ac86596b1d82ca04
associaasppibra <associaasppibra@gmail.com>
2026-02-05 10:39:11 +0000

    chore: Clean and optimize repository structure

--- update_users_table.sql
+++
@@ -1,4 +0,0 @@
--- Adiciona as colunas que faltam na tabela users
-ALTER TABLE users ADD COLUMN firstName TEXT;
-ALTER TABLE users ADD COLUMN lastName TEXT;
-ALTER TABLE users ADD COLUMN role TEXT DEFAULT 'citizen';
